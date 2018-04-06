b871d8f873b32692288c63bc32427c18e397a403
ripee <ripee.dev@gmail.com>
2018-04-06 20:39:21 +0200

    Update Android.mk

--- init/Android.mk
+++ init/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_trlte.cpp
-LOCAL_MODULE := libinit_trlte
+LOCAL_SRC_FILES := init_tblte.cpp
+LOCAL_MODULE := libinit_tblte
 
 include $(BUILD_STATIC_LIBRARY)
